5cbd27e609b438624dbccadfa7ab0611779937f6
felix <felixenchiparambil@hotmail.com>
2017-12-03 09:29:52 +0100

    created main UI and new/edit gadget window

---
+++ ch.hsr.wpf.gadgeothek.ui/NewEditGadget.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ch.hsr.wpf.gadgeothek.ui
+{
+    /// <summary>
+    /// Interaction logic for NewEditGadget.xaml
+    /// </summary>
+    public partial class NewEditGadget : Window
+    {
+        public NewEditGadget()
+        {
+            InitializeComponent();
+        }
+    }
+}
