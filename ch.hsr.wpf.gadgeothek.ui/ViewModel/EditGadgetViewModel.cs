1077be81e2d0a9fb3ba35064fd9036cd0e45572e
felix <felixenchiparambil@hotmail.com>
2017-12-15 12:14:34 +0100

    add gadget edit implementation

---
+++ ch.hsr.wpf.gadgeothek.ui/ViewModel/EditGadgetViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ch.hsr.wpf.gadgeothek.domain;
+using ch.hsr.wpf.gadgeothek.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ch.hsr.wpf.gadgeothek.ui.ViewModel
+{
+    class EditGadgetViewModel
+    {
+        public event EventHandler CanClose;
+
+        private LibraryAdminService Service { get; set; }
+
+        public Gadget EditGadget { get; set; } = new Gadget();
+
+        public ICommand ConfirmEditCommand { get; set; }
+
+        public ICommand CancelEditCommand { get; set; }
+
+        public EditGadgetViewModel(Gadget gadget)
+        {
+            var url = System.Configuration.ConfigurationManager.AppSettings["serverGadgeothek"];
+            Service = new LibraryAdminService(url);
+
+            EditGadget = gadget;
+
+            ConfirmEditCommand = new RelayCommand.RelayCommand<Object>((o) => ConfirmEdit());
+            CancelEditCommand = new RelayCommand.RelayCommand<Object>((o) => CancelEdit());
+        }
+
+        public void ConfirmEdit()
+        {
+            if (Service.UpdateGadget(EditGadget))
+            {
+                MessageBox.Show("Update was Successful");
+                CanClose?.Invoke(this, new EventArgs());
+            }
+        }
+
+        public void CancelEdit()
+        {
+            CanClose?.Invoke(this, new EventArgs());
+        }
+    }
+}
